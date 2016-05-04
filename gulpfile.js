9fed69c5a8c408aedf714e90f0c683bd2ff8ebe8
yapadesouci <franck.anso@gmail.com>
2016-05-04 17:57:01 +0200

    Rollback gulpfile because not compatible with travis.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const excludeGitignore = require('gulp-exclude-gitignore');
-const mocha = require('gulp-mocha');
-const istanbul = require('gulp-istanbul');
-const nsp = require('gulp-nsp');
-const plumber = require('gulp-plumber');
-const coveralls = require('gulp-coveralls');
-const babel = require('gulp-babel');
-const del = require('del');
-const isparta = require('isparta');
+var path = require('path');
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var excludeGitignore = require('gulp-exclude-gitignore');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var nsp = require('gulp-nsp');
+var plumber = require('gulp-plumber');
+var coveralls = require('gulp-coveralls');
+var babel = require('gulp-babel');
+var del = require('del');
+var isparta = require('isparta');
 
 // Initialize the babel transpiler so ES2015 files gets compiled
 // when they're loaded
 require('babel-core/register');
 
-gulp.task('static', () => {
+gulp.task('static', function () {
   return gulp.src('**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
@@ -23,11 +23,11 @@ gulp.task('static', () => {
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('nsp', cb => {
+gulp.task('nsp', function (cb) {
   nsp({package: path.resolve('package.json')}, cb);
 });
 
-gulp.task('pre-test', () => {
+gulp.task('pre-test', function () {
   return gulp.src('lib/**/*.js')
     .pipe(excludeGitignore())
     .pipe(istanbul({
@@ -37,26 +37,26 @@ gulp.task('pre-test', () => {
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'], cb => {
-  let mochaErr;
+gulp.task('test', ['pre-test'], function (cb) {
+  var mochaErr;
 
   gulp.src('test/**/*.js')
     .pipe(plumber())
     .pipe(mocha({reporter: 'spec'}))
-    .on('error', err => {
+    .on('error', function (err) {
       mochaErr = err;
     })
     .pipe(istanbul.writeReports())
-    .on('end', () => {
+    .on('end', function () {
       cb(mochaErr);
     });
 });
 
-gulp.task('watch', () => {
+gulp.task('watch', function () {
   gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
 });
 
-gulp.task('coveralls', ['test'], () => {
+gulp.task('coveralls', ['test'], function () {
   if (!process.env.CI) {
     return;
   }
@@ -65,13 +65,13 @@ gulp.task('coveralls', ['test'], () => {
     .pipe(coveralls());
 });
 
-gulp.task('babel', ['clean'], () => {
+gulp.task('babel', ['clean'], function () {
   return gulp.src('lib/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', () => {
+gulp.task('clean', function () {
   return del('dist');
 });
 
